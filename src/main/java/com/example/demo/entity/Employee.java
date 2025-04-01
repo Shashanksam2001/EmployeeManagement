03918c5511ae9ff91b760197d88fdd4618d7e761
Shashanksam2001 <mangalashashank2001@gmail.com>
2025-04-02 01:52:48 +0530

    creatingjobcontroller

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -39,98 +39,128 @@ public class Employee {
     @ManyToOne
     @JoinColumn(name = "department_id", nullable = false)
     private Department department;
-
-	public Department getDepartment() {
-		return department;
+    	
+    
+  @Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", phone=" + phone + ", dateOfJoining=" + dateOfJoining + ", salary=" + salary + ", department="
+				+ department + ", job=" + job + "]";
 	}
 
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
 
-	public Employee() {
+public Employee() {
 		super();
 	}
 
-	public int getId() {
+
+public Employee(int id, String firstName, String lastName, String email, String phone, Date dateOfJoining,
+			double salary, Department department, Job job) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		this.dateOfJoining = dateOfJoining;
+		this.salary = salary;
+		this.department = department;
+		this.job = job;
+	}
+
+
+public int getId() {
 		return id;
 	}
+
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+
 	public String getLastName() {
 		return lastName;
 	}
+
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+
 	public String getEmail() {
 		return email;
 	}
+
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+
 	public String getPhone() {
 		return phone;
 	}
+
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+
 	public Date getDateOfJoining() {
 		return dateOfJoining;
 	}
+
 
 	public void setDateOfJoining(Date dateOfJoining) {
 		this.dateOfJoining = dateOfJoining;
 	}
 
+
 	public double getSalary() {
 		return salary;
 	}
+
 
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", phone=" + phone + ", dateOfJoining=" + dateOfJoining + ", salary=" + salary + "]";
+
+	public Department getDepartment() {
+		return department;
 	}
 
-	public Employee(int id, String firstName, String lastName, String email, String phone, Date dateOfJoining,
-			double salary) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.phone = phone;
-		this.dateOfJoining = dateOfJoining;
-		this.salary = salary;
+
+	public void setDepartment(Department department) {
+		this.department = department;
 	}
 
-   
-//
-//    @ManyToOne
-//    @JoinColumn(name = "job_id", nullable = false)
-   
 
-		
+	public Job getJob() {
+		return job;
+	}
+
+
+	public void setJob(Job job) {
+		this.job = job;
+	}
+
+
+@ManyToOne
+  @JoinColumn(name = "job_id", nullable = false)
+    private Job job;
+	
 }
--- src/main/java/com/example/demo/entity/Job.java
+++ src/main/java/com/example/demo/entity/Job.java
@@ -1,106 +1,106 @@
-//package com.example.demo.entity;
-//
-//import java.util.List;
-//
-//import jakarta.persistence.CascadeType;
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.OneToMany;
-//import jakarta.persistence.Table;
-//
-//@Entity
-//
-//@Table(name = "jobs")
-//public class Job {
-//	@Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-////    private int id;
-////
-////    @Column(name = "title", unique = true, nullable = false, length = 100)
-////    private String title;
-////
-////    @Column(name = "description", length = 255)
-////    private String description;
-////
-////    @Column(name = "min_salary")
-////    private double minSalary;
-////
-////    @Column(name = "max_salary")
-////    private double maxSalary;
-////
-////    @OneToMany(mappedBy = "job", cascade = CascadeType.ALL)
-////    private List<Employee> employees;
-//	public Job() {
-//		super();
-//	}
-//
-//	public Job(int id, String title, String description, double minSalary, double maxSalary, List<Employee> employees) {
-//		super();
-//		this.id = id;
-//		this.title = title;
-//		this.description = description;
-//		this.minSalary = minSalary;
-//		this.maxSalary = maxSalary;
-//		this.employees = employees;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Job [id=" + id + ", title=" + title + ", description=" + description + ", minSalary=" + minSalary
-//				+ ", maxSalary=" + maxSalary + ", employees=" + employees + "]";
-//	}
-//
-//	public int getId() {
-//		return id;
-//	}
-//
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//
-//	public String getTitle() {
-//		return title;
-//	}
-//
-//	public void setTitle(String title) {
-//		this.title = title;
-//	}
-//
-//	public String getDescription() {
-//		return description;
-//	}
-//
-//	public void setDescription(String description) {
-//		this.description = description;
-//	}
-//
-//	public double getMinSalary() {
-//		return minSalary;
-//	}
-//
-//	public void setMinSalary(double minSalary) {
-//		this.minSalary = minSalary;
-//	}
-//
-//	public double getMaxSalary() {
-//		return maxSalary;
-//	}
-//
-//	public void setMaxSalary(double maxSalary) {
-//		this.maxSalary = maxSalary;
-//	}
-//
-//	public List<Employee> getEmployees() {
-//		return employees;
-//	}
-//
-//	public void setEmployees(List<Employee> employees) {
-//		this.employees = employees;
-//	}
-//
-//	
-//
-//}
+package com.example.demo.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+
+@Table(name = "jobs")
+public class Job {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title", unique = true, nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @Column(name = "min_salary")
+    private double minSalary;
+
+   @Column(name = "max_salary")
+    private double maxSalary;
+
+    @OneToMany(mappedBy = "job", cascade = CascadeType.ALL)
+    private List<Employee> employees;
+	public Job() {
+		super();
+	}
+
+	public Job(int id, String title, String description, double minSalary, double maxSalary, List<Employee> employees) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.minSalary = minSalary;
+		this.maxSalary = maxSalary;
+		this.employees = employees;
+	}
+
+	@Override
+	public String toString() {
+		return "Job [id=" + id + ", title=" + title + ", description=" + description + ", minSalary=" + minSalary
+				+ ", maxSalary=" + maxSalary + ", employees=" + employees + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getMinSalary() {
+		return minSalary;
+	}
+
+	public void setMinSalary(double minSalary) {
+		this.minSalary = minSalary;
+	}
+
+	public double getMaxSalary() {
+		return maxSalary;
+	}
+
+	public void setMaxSalary(double maxSalary) {
+		this.maxSalary = maxSalary;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+	
+
+}
